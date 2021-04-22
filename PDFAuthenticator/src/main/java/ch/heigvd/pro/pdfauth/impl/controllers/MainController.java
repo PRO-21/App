5b735c0876da9ff5bab61d2f96eb6fdd8e0867b8
Nicolas Ogi <nicolas.ogi@heig-vd.ch>
2021-04-22 19:45:23 +0200

    Modification comportement JsonObject

--- PDFAuthenticator/src/main/java/ch/heigvd/pro/pdfauth/impl/controllers/MainController.java
+++ PDFAuthenticator/src/main/java/ch/heigvd/pro/pdfauth/impl/controllers/MainController.java
@@ -19,8 +19,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -161,7 +161,7 @@ public class MainController implements Initializable {
 
                 if (HttpCode == HttpURLConnection.HTTP_OK) {
                     String certificateID = obj.getJSONObject("data").getString("idCertificat");
-                    System.out.println("URL : https://pro.simeunovic.ch:8022/protest/view/scan.php/?id=" + certificateID);
+                    System.out.println("URL : https://pro.simeunovic.ch:8022/protest/view/scan.php?id=" + certificateID);
                 }
                 else {
                     Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -196,13 +196,28 @@ public class MainController implements Initializable {
     private String prepareFieldsToSend() {
 
         JSONObject obj = new JSONObject();
+
+        // Comme à la base les JSONObject sont des HashMap qui ne sont pas ordonnées pour des raisons de performance,
+        // les champs à protéger ne sont pas disposés dans l'ordre dans lequel ils ont été détectés dans le PDF. Ce qui
+        // peut être embêtant lors de la vérification entre le formulaire papier et les champs affichés sur le site web.
+        // Grâce à l'API reflect inclue dans Java, il est possible de modifier le comportement de la classe JSONObject
+        // pour qu'elle utilise un autre type de HashMap qui lui, est ordonné.
+        try {
+            java.lang.reflect.Field changeMap = obj.getClass().getDeclaredField("map");
+            changeMap.setAccessible(true); // permet de rendre accessible la Map à modifier
+            changeMap.set(obj, new LinkedHashMap<>()); // Remplacement de HashMap par LinkedHashMap
+            changeMap.setAccessible(false);
+        }
+        catch (IllegalAccessException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         obj.put("dateSignature", dateFormat.format(new Date()));
 
         for (Field field : fields) {
 
             if (field.getIsProtected().isSelected())
-                // Comme les JSONObject sont des unordered sets, il ne conserve pas l'ordre des champs
                 obj.put(field.getFieldName(), field.getValue());
         }
         return obj.toString();
